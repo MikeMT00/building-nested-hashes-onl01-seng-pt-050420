c1d93b788423db7015916715f0589f85b592d3cc
Michael Thomas <michaelthomas0611@gmail.com>
2020-05-17 13:28:54 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,17 +1,8 @@
 def second_challenge
-  :montague => {
-     :patriarch => {},
-     :matriarch => {},
-     :hero => {},
-     :hero_friends => []
-  },
-  :capulet => {
-     :patriarch => {},
-     :matriarch => {},
-     :heroine => {},
-     :heroine_friends => []
-  }
-}
-
-
+  epic_tragedy = {
+    :montague => {:patriarch => {}, :matriarch => {}, :hero => {}, :hero_friends => []
+      }, 
+    :capulet => {:patriarch => {}, :matriarch => {}, :heroine =>, :heroine_friends => []
+      }
+    }  
 end
